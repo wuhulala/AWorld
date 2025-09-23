04cc44d37101b8a58ad18b0f2055457b01e0d53c
Eric <wenlong.yanwl@antgroup.com>
2025-09-23 19:20:16 +0800

    commit codes

--- env/run-local.sh
+++ env/run-local.sh
@@ -3,4 +3,7 @@ cd "$(dirname "$0")"
 
 sh gaia-mcp-server/run.sh && \
 
+export MCP_SERVER_URL=http://localhost:8080/mcp
+export MCP_SERVER_TOKEN=1234567890
+
 open 'http://localhost:5901/vnc.html?autoconnect=true'
