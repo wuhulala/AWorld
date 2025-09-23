8cb03308e8a020a6073c56657105f8918bb7a665
Eric <wenlong.yanwl@antgroup.com>
2025-09-23 19:32:43 +0800

    commit codes

--- env/gaia-mcp-server/run.sh
+++ env/gaia-mcp-server/run.sh
@@ -5,6 +5,20 @@ sh ../mcp-server-base/build-image.sh && \
 
 docker compose up --build --force-recreate -d && \
 
-sleep 3 && \
+export MCP_SERVER_URL=http://localhost:8080/mcp && \
 
-open 'http://localhost:5901/vnc.html?autoconnect=true'
+cat << 'EOF'
+✅ Start mcp server success
+Please using the following config to connect to the mcp server:
+```json
+{
+  "gaia-mcp-server": {
+    "type": "streamable_http",
+    "url": "http://localhost:8000/mcp",
+    "timeout": 6000,
+    "sse_read_timeout": 6000,
+    "client_session_timeout_seconds": 6000
+  }
+}
+```
+EOF
--- env/run-local.sh
+++ env/run-local.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
 cd "$(dirname "$0")"
 
-sh gaia-mcp-server/run.sh && \
-
-export MCP_SERVER_URL=http://localhost:8080/mcp
-export MCP_SERVER_TOKEN=1234567890
-
-open 'http://localhost:5901/vnc.html?autoconnect=true'
+sh gaia-mcp-server/run.sh
